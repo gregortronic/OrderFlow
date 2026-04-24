5e397c820ad6659ff34753bde76836ffae4898a6
gregortronic <rozkin@internet.ru>
2026-04-24 18:55:20 +0500

    feat(application): DI settings

--- OrderFlow.Application/DependencyInjection.cs
+++ OrderFlow.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OrderFlow.Application.Abstractions.Persistence;
+using OrderFlow.Application.Documents;
 using OrderFlow.Application.Orders;
 
 namespace OrderFlow.Application;
@@ -8,6 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IOrderApplicationService, OrderApplicationService>();
+        services.AddScoped<IDocumentJobApplicationService, DocumentJobApplicationService>();
 
         return services;
     }
